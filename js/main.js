7c03aae6e11595cfa89990229b1f9d07adae2ff1
Brittany Robertson <brittanyrob@live.ca>
2014-11-04 09:45:05 -0500

    added work animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var $win = $(window);
 var $aboutmeSection =$('.aboutme');
+var $workItem=$(".work-item");
+var $workClose = $('.work-close');
 
 
 $win.on('scroll', function () {
@@ -9,9 +11,16 @@ $win.on('scroll', function () {
 	$aboutmeSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 });
 
-
+/*
 $aboutmeSection.waypoint(function () {
 	$aboutme.addClass('js-aboutme-animate');
 	
 }, { offset: '50%' });
+*/
+$workItem.on("click",function () {
+	$(this).siblings(".work-info").addClass("on");
+});
 
+$workClose.on('click', function () {
+	$(this).parent().removeClass('on');
+});
