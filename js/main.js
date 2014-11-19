1f3f9182158bfb638d79daa7ee12c0a6ee79ce79
Brittany Robertson <brittanyrob@live.ca>
2014-11-19 11:20:20 -0500

    fixed top image

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var $aboutmeSection =$('.aboutme');
 var $workItem=$(".work-item");
 var $workClose = $('.work-close');
 var $workcloseReverse = $('.work-close-reverse');
+var $iconTop = $('.icon-top');
 
 
 $win.on('scroll', function () {
@@ -28,4 +29,24 @@ $workClose.on("click", function () {
 
 $workcloseReverse.on("click", function () {
 	$(this).parent().removeClass('on');
+});
+
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.iconTop').fadeIn();
+		} else {
+			$('.iconTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.iconTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
 });
