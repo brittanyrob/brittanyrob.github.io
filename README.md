0c70185ae81da5f741c1eca189e575a3d302720e
Brittany Robertson <brittanyrob@live.ca>
2015-03-09 12:18:02 -0400

    new content

--- README.md
+++
@@ -1,2 +0,0 @@
-brittanyrob.github.io
-=====================
--- js/main.js
+++ js/main.js
@@ -1,207 +1,263 @@
-var $win = $(window);
-var $aboutmeSection =$('.aboutme');
-var $workItem=$(".work-item");
-var $workClose = $('.work-close');
-var $workcloseReverse = $('.work-close-reverse');
-var $iconTop = $('.icon-top');
-var $video = $('.video');
-
-
-$win.on('scroll', function () {
-	var scrollPos = $win.scrollTop();
-	
-	/*console.log(scrollPos);  For debugging*/
-	$aboutmeSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
-});
-
 /*
-$aboutmeSection.waypoint(function () {
-	$aboutme.addClass('js-aboutme-animate');
-	
-}, { offset: '50%' });
+	01. Smooth scrolling
+	02. FitText
+	03. Magnific Popup
+	04. Sliders
+	05. Fading divs
+	06. Contactform
+	07. Menu toggle
+	08. Fixed menu
+	09. Equal height
 */
-$workItem.on("click",function () {
-	$(this).siblings(".work-info").addClass("on");
-});
-
-$workClose.on("click", function () {
-	$(this).parent().removeClass('on');
-});
 
 
-/* Slideshow */
-
-
-var makeBSS = function (el, options) {
-    var $slideshows = document.querySelectorAll(el), // a collection of all of the slideshow
-        $slideshow = {},
-        Slideshow = {
-            init: function (el, options) {
-                this.counter = 0; // to keep track of current slide
-                this.el = el; // current slideshow container    
-                this.$items = el.querySelectorAll('figure'); // a collection of all of the slides, caching for performance
-                this.numItems = this.$items.length; // total number of slides
-                options = options || {}; // if options object not passed in, then set to empty object 
-                options.auto = options.auto || false; // if options.auto object not passed in, then set to false
-                this.opts = {
-                    auto: (typeof options.auto === "undefined") ? false : options.auto,
-                    speed: (typeof options.auto.speed === "undefined") ? 1500 : options.auto.speed,
-                    pauseOnHover: (typeof options.auto.pauseOnHover === "undefined") ? false : options.auto.pauseOnHover,
-                    fullScreen: (typeof options.fullScreen === "undefined") ? false : options.fullScreen,
-                    swipe: (typeof options.swipe === "undefined") ? false : options.swipe
-                };
-                
-                this.$items[0].classList.add('bss-show'); // add show class to first figure 
-                this.injectControls(el);
-                this.addEventListeners(el);
-                if (this.opts.auto) {
-                    this.autoCycle(this.el, this.opts.speed, this.opts.pauseOnHover);
-                }
-                if (this.opts.fullScreen) {
-                    this.addFullScreen(this.el);
-                }
-                if (this.opts.swipe) {
-                    this.addSwipe(this.el);
-                }
-            },
-            showCurrent: function (i) {
-                // increment or decrement this.counter depending on whether i === 1 or i === -1
-                if (i > 0) {
-                    this.counter = (this.counter + 1 === this.numItems) ? 0 : this.counter + 1;
-                } else {
-                    this.counter = (this.counter - 1 < 0) ? this.numItems - 1 : this.counter - 1;
-                }
-
-                // remove .show from whichever element currently has it 
-                // http://stackoverflow.com/a/16053538/2006057
-                [].forEach.call(this.$items, function (el) {
-                    el.classList.remove('bss-show');
-                });
-  
-                // add .show to the one item that's supposed to have it
-                this.$items[this.counter].classList.add('bss-show');
-            },
-            injectControls: function (el) {
-            // build and inject prev/next controls
-                // first create all the new elements
-                var spanPrev = document.createElement("span"),
-                    spanNext = document.createElement("span"),
-                    docFrag = document.createDocumentFragment();
-        
-                // add classes
-                spanPrev.classList.add('bss-prev');
-                spanNext.classList.add('bss-next');
-        
-                // add contents
-                spanPrev.innerHTML = '&laquo;';
-                spanNext.innerHTML = '&raquo;';
-                
-                // append elements to fragment, then append fragment to DOM
-                docFrag.appendChild(spanPrev);
-                docFrag.appendChild(spanNext);
-                el.appendChild(docFrag);
-            },
-            addEventListeners: function (el) {
-                var that = this;
-                el.querySelector('.bss-next').addEventListener('click', function () {
-                    that.showCurrent(1); // increment & show
-                }, false);
-            
-                el.querySelector('.bss-prev').addEventListener('click', function () {
-                    that.showCurrent(-1); // decrement & show
-                }, false);
-                
-                el.onkeydown = function (e) {
-                    e = e || window.event;
-                    if (e.keyCode === 37) {
-                        that.showCurrent(-1); // decrement & show
-                    } else if (e.keyCode === 39) {
-                        that.showCurrent(1); // increment & show
-                    }
-                };
-            },
-            autoCycle: function (el, speed, pauseOnHover) {
-                var that = this,
-                    interval = window.setInterval(function () {
-                        that.showCurrent(1); // increment & show
-                    }, speed);
-                
-                if (pauseOnHover) {
-                    el.addEventListener('mouseover', function () {
-                        interval = clearInterval(interval);
-                    }, false);
-                    el.addEventListener('mouseout', function () {
-                        interval = window.setInterval(function () {
-                            that.showCurrent(1); // increment & show
-                        }, speed);
-                    }, false);
-                } // end pauseonhover
-                
-            },
-            addFullScreen: function(el){
-                var that = this,
-                fsControl = document.createElement("span");
-                
-                fsControl.classList.add('bss-fullscreen');
-                el.appendChild(fsControl);
-                el.querySelector('.bss-fullscreen').addEventListener('click', function () {
-                    that.toggleFullScreen(el);
-                }, false);
-            },
-            addSwipe: function(el){
-                var that = this,
-                    ht = new Hammer(el);
-                ht.on('swiperight', function(e) {
-                    that.showCurrent(-1); // decrement & show
-                });
-                ht.on('swipeleft', function(e) {
-                    that.showCurrent(1); // increment & show
-                });
-            },
-           
-            
-        }; // end Slideshow object 
-        
-    // make instances of Slideshow as needed
-    [].forEach.call($slideshows, function (el) {
-        $slideshow = Object.create(Slideshow);
-        $slideshow.init(el, options);
-    });
-};
-var opts = {
-    auto : {
-        speed : 5000, 
-        pauseOnHover : true
-    },
-    fullScreen : true, 
-    swipe : true
-};
-makeBSS('.demo1', opts);
-makeBSS('.demo2', opts);
-makeBSS('.demo3', opts);
-
-$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
-
-$('.video').parallax({
-speed : 0.15
-});
-
-$('.footer-image').parallax({
-speed : 0.15
-});
-
-$('.nav-mobile').bind('click', function(e) {
+// 01. Smooth scrolling - Smooth scroll after clicking an element with the class 'smoothscroll'
+$(function(){
+	$('.smoothscroll').bind('click.smoothscroll',function (e){
 		e.preventDefault();
-		if ($('nav').is(":visible")) 
-		{
-			$('nav').stop().slideUp();
-		}
-		else 
-		{
-			$('nav').stop().slideDown();
-		}
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+			'scrollTop': $target.offset().top-0
+        }, 900, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+});
+
+
+// 02. FitText
+$(window).load(function(){
+	setTimeout(function(){
+		$('h1.fittext').fitText(1, { minFontSize: '50px', maxFontSize: '100px' });
+		$('h2.fittext').fitText(1, { minFontSize: '40px', maxFontSize: '80px' });
+		$('h3.fittext').fitText(1, { minFontSize: '30px', maxFontSize: '60px' });
+		$('h4.fittext').fitText(1, { minFontSize: '20px', maxFontSize: '40px' });
+		$('h5.fittext').fitText(1, { minFontSize: '15px', maxFontSize: '30px' });
+		$('h6.fittext').fitText(1, { minFontSize: '10px', maxFontSize: '20px' });
+	}, 200);
+});
+
+$('h1.fittext').fitText(1, { minFontSize: '50px', maxFontSize: '100px' });
+$('h2.fittext').fitText(1, { minFontSize: '40px', maxFontSize: '80px' });
+$('h3.fittext').fitText(1, { minFontSize: '30px', maxFontSize: '60px' });
+$('h4.fittext').fitText(1, { minFontSize: '20px', maxFontSize: '40px' });
+$('h5.fittext').fitText(1, { minFontSize: '15px', maxFontSize: '30px' });
+$('h6.fittext').fitText(1, { minFontSize: '10px', maxFontSize: '20px' });
+
+
+// 03. Magnific Popup
+// Image popup
+$('.popup').magnificPopup({ 
+	type: 'image',
+	fixedContentPos: false,
+	fixedBgPos: false,
+	removalDelay: 300,
+	mainClass: 'mfp-fade'
+});
+
+
+// YouTube, Vimeo and Google Maps popup
+$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	disableOn: 700,
+	type: 'iframe',
+	fixedContentPos: false,
+	fixedBgPos: false,
+	removalDelay: 300,
+	mainClass: 'mfp-fade',
+	preloader: false
+});
+
+
+// Gallery popup
+$('.popup-gallery').magnificPopup({
+	type: 'image',
+	gallery:{
+		enabled:true
+	},
+	fixedContentPos: false,
+	fixedBgPos: false,
+	removalDelay: 300,
+	mainClass: 'mfp-fade'
+});
+
+
+// Gallery link - Use the gallery-link to create a link to a gallery
+$('.gallery-link').on('click', function () {
+	$(this).next().magnificPopup('open');
+});
+
+// Gallery - Add every image you want to become visible in a popup inside a div with the gallery class
+$('.gallery').each(function () {
+	$(this).magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true
+		},
+		fixedContentPos: false,
+		fixedBgPos: false,
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
+	});
+});
+
+
+// Soundcloud popup
+$('.popup-soundcloud').magnificPopup({ 
+	type: 'iframe',
+	mainClass: 'soundcloud-popup'
+});
+
+
+// 04. Sliders
+$(window).load(function(){
+
+	// Featured work slider
+	$('#featured-work-slider').bxSlider({
+		auto: false,
+		mode: 'fade',
+		pager: true,
+		controls: true,
+		nextText: '',
+		prevText: ''
 	});
 	
+	// Project page slider
+	$('#project-page-slider').bxSlider({
+		auto: false,
+		mode: 'fade',
+		pager: true,
+		controls: true,
+		nextText: '',
+		prevText: ''
+	});
+
+	// Quote slider
+	var quoteslider = $('#quote-slider').bxSlider({
+		auto: false,
+		responsive: true,
+		adaptiveHeight: true,
+		mode: 'fade',
+		pager: false,
+		controls: false
+	});
+
+	$('#quote-next').click(function(){
+		quoteslider.goToNextSlide();
+		return false;
+	});
+
+	$('#quote-prev').click(function(){
+		quoteslider.goToPrevSlide();
+		return false;
+	});
+	
+});
+
+
+// 05. Fading divs - Fade a div except the one that's hovered
+if($('html').hasClass('no-touch')){
+	$('.fade-it, .image-thumb, .service-item').hover(function(){
+		$(this).siblings().addClass('fade');
+	}, function(){
+		$(this).siblings().removeClass('fade');
+	});
+}
+
+
+// 06. Contactform
+$(function(){
+
+	$('#contactform').submit(function(){
+
+		var action = $(this).attr('action');
+
+		$('#message').slideUp(300,function() {
+		$('#message').hide();
+
+ 		$('#submit')
+			.after('<img src="images/status.gif" class="loader">')
+			.attr('disabled','disabled');
+
+		$.post(action, {
+			name: $('#name').val(),
+			email: $('#email').val(),
+			phone: $('#phone').val(),
+			comments: $('#comments').val(),
+			verify: $('#verify').val()
+		},
+			function(data){
+				document.getElementById('message').innerHTML = data;
+				$('#message').slideDown(300);
+				$('#contactform img.loader').fadeOut(300,function(){$(this).remove()});
+				$('#submit').removeAttr('disabled');
+				if(data.match('success') != null) $('#contactform').slideUp(300);
+			}
+		);
+
+		});
+
+		return false;
+
+	});
+
+});
+
+
+// 07. Menu toggle
+$(function(){
+    $('#toggle').click(function (e){
+		e.stopPropagation();
+    });
+	$('html').click(function (e){
+		if (!$('.toggle').is($(e.target))){
+			$('#toggle').prop("checked", false);
+		}
+	});
+});
+
+
+// 08. Fixed menu - Fix the menu to the top after you scroll past it
+$(window).load(function(){
+	'use strict';
+	$(".fixedmenu").sticky({ topSpacing: 0 });
+});
+
+
+/* 09. Use the .equal class on a row if you want the columns to be equal in height */
+$('.equal').children('.columns').equalizeHeight();
+$( window ).resize( function() {
+	$('.equal').children('.columns').equalizeHeight();
+	setTimeout( function() {
+		$('.equal').children('.columns').equalizeHeight();
+	}, 100 );
+	setTimeout( function() {
+		$('.equal').children('.columns').equalizeHeight();
+	}, 400 );
+	setTimeout( function() {
+		$('.equal').children('.columns').equalizeHeight();
+	}, 1400 );
+	setTimeout( function() {
+		$('.equal').children('.columns').equalizeHeight();
+	}, 2400 );
+});
+setTimeout( function() {
+	$(window).trigger('resize scroll');
+}, 1000 );
+setTimeout( function() {
+	$(window).trigger('resize scroll');
+}, 3000 );
+$( window ).load( function() {
+	$( '.equal' ).children('.columns').equalizeHeight();
+	$( window ).trigger('resize scroll');
+	setTimeout( function() {
+		$('.equal').children('.columns').equalizeHeight();
+	}, 1000 );
+	setTimeout( function() {
+		$('.equal').children('.columns').equalizeHeight();
+	}, 1300 );
+});
